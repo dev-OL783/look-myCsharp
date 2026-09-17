e4b7224b07694247eca08706c3618f9daed303f4
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 14:23:50 +1000

    Add files via upload

---
+++ week1/T01Q10___Locate_Symbol/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace T01Q10___Locate_Symbol;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        Console.WriteLine("Please enter a line of text:");
+        string lineOfText = Console.ReadLine();
+        int firstPos = lineOfText.IndexOf('@');
+        int lastPos = lineOfText.LastIndexOf('@');
+
+        Console.WriteLine("The first and last positions of '@' are {0} and {1}, respectively.",
+            firstPos, lastPos);
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");   
+    }
+}
