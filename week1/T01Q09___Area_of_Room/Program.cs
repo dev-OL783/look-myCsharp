c218a57f033c27d3128c77b433db5c8779f3a617
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 13:19:03 +1000

    Add files via upload

---
+++ week1/T01Q09___Area_of_Room/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace T01Q09___Area_of_Room;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        Console.WriteLine("Please enter the width of the room, in metres: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+        
+
+        Console.WriteLine("Please enter the length of the room, in metres:");
+        double length = Convert.ToDouble(Console.ReadLine());
+        
+        double area = length * width;
+
+
+        Console.WriteLine(area);
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+    }
+}
