0b708a464a75840320d87a239b7dc6f0ac56bf08
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 13:03:35 +1000

    Add files via upload

---
+++ week1/T01Q07___Echo_Trimmed_Name/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace T01Q07___Echo_Trimmed_Name;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        // Display prompt
+        Console.WriteLine("Hello there, what is your name?");
+        // Read name, trim leading and trailing white space
+        string name = Console.ReadLine();
+        name = name.Trim();
+        // Display message including name
+        Console.WriteLine("hi " + name + " yep");
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+    }
+} 
