e7c421ae1ec1ebe5aa8487dfe32a723a11d695b4
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 13:03:00 +1000

    Add files via upload

---
+++ week1/T01Q06___Echo_Name/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T01Q06___Echo_Name;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here:
+        //  Display prompt
+        Console.WriteLine("Hi there, what is your favorite icecream flavor?");
+        //  Read name
+        string flavor = Console.ReadLine();
+        //  Display message including name
+        Console.WriteLine("Nice, your fav flavor is " + flavor + ".");
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+    }
+}
