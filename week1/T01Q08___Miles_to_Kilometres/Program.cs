faea1f0725c80e4b37c507570e91a27b97c0a811
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 13:08:21 +1000

    Add files via upload

---
+++ week1/T01Q08___Miles_to_Kilometres/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace T01Q08___Miles_to_Kilometres;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("What the Hell is a kilometerrrr.");
+        
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        // Display prompt
+        Console.WriteLine("Please supply the distance in miles: ");
+        // Read number of miles
+        
+        // double miles = Console.Readline();
+        double miles = double.Parse(Console.ReadLine());
+
+        // Compute equivalent number of kilometres
+        double km = miles * 1.609344;
+        // Display message involving miles and kilometres
+        Console.WriteLine(miles + " miles -> " + km + "kms.");
+
+        // Keep the following line intact 
+        Console.WriteLine("===========================");
+    }
+}
