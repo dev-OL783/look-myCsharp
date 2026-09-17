13f25f49f04e8550dafc55ee711f7e6aed9f01e3
Oscar <313242658+dev-OL783@users.noreply.github.com>
2026-09-17 13:00:09 +1000

    Add files via upload

---
+++ T01Q05___Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace T01Q05___Hello_World;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Keep this line intact 
+        Console.WriteLine("===========================");
+
+        // Insert your solution here.
+        Console.WriteLine("Welcome to my Domain!_GLHF_");
+
+        // Keep this line intact
+        Console.WriteLine("===========================");
+    }
+}
